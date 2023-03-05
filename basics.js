0e11db5e2c72b60d85d37b2447c32cce2c63ecc2
Razyapova Aliya <a.raziapova@mail.ru>
2023-03-05 20:47:41 +0300

    add strings

---
+++ basics.js
@@ -0,0 +1,20 @@
+console.log('Hello world');
+const a = 1; let b = 1; b = 2
+
+if (a == 1) console.log(123)
+
+console.log(a, b, typeof a);
+
+let str = 'Hello';
+console.log(str);
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str[0]);
+console.log(str.length);
+console.log(str.indexOf("e"));
+console.log(str.indexOf("H"));
+console.log(str.indexOf("a"));
+
+console.log("Still string");
+console.log(`still string ${str} ${a}`);
+console.log(typeof "str");
