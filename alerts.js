007adcad89e9a384e34302d761c3d6648ef9a370
Razyapova Aliya <a.raziapova@mail.ru>
2023-03-05 21:07:55 +0300

    add alerts

---
+++ alerts.js
@@ -0,0 +1,16 @@
+// alert("Alert!!!")
+
+// if (confirm("Уверен?")) {
+//     console.log("да");
+// }
+// else {
+//     console.log("нет");
+// }
+
+const name = prompt("Напиши имя");
+if (name) {
+    console.log(name);
+}
+else {
+    console.log("не ввел имя");
+}
