9f1003791d4d03eac856cc267f4e3f90dd5a8f5c
Razyapova Aliya <a.raziapova@mail.ru>
2023-03-06 22:56:24 +0300

    add form handler as separated function

--- fetchExamples.js
+++ fetchExamples.js
@@ -1,26 +1,27 @@
+function formHandler(event) {
+    event.preventDefault();
+    const formdata = new FormData(event.target);
+    const username = formdata.get("username");
+    console.log("start request");
+    const promise = fetch(`https://api.github.com/users/${username}`);
+    console.log(promise)
+    promise
+        .then(function (result) {
+            console.log(promise);
+            console.log("response");
+            return result.json();
+        })
+        .then(function (content) {
+            console.log("content");
+            console.log(content);
+        })
+        .catch(function (result) {
+            console.log(promise);
+            console.error(result);
+        });
+    console.log("123");
+}
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("#githubForm");
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        const formdata = new FormData(form);
-        const username = formdata.get("username");
-        console.log("start request");
-        const promise = fetch(`https://api.github.com/users/${username}`);
-        console.log(promise)
-        promise
-            .then(function (result) {
-                console.log(promise);
-                console.log("response");
-                return result.json();
-            })
-            .then(function (content) {
-                console.log("content");
-                console.log(content);
-            })
-            .catch(function (result) {
-                console.log(promise);
-                console.error(result);
-            });
-        console.log("123");
-    });
+    form.addEventListener("submit", formHandler);
 })
